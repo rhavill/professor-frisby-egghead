44a83d59e8b616280f340acb3b31ade9f29794a9
Ricardo Havill <ricardo.havill@mjfreeway.com>
2017-02-19 00:55:04 -0500

    Added Task example.

---
+++ 12-task.js
@@ -0,0 +1,56 @@
+const Task = require('data.task');
+
+// Task.of(1)
+// .fork(
+// 	e => console.log('error', e),
+// 	x => console.log('success', x)		
+// );
+
+// Task.rejeced(1)
+// .fork(
+// 	e => console.log('error', e),
+// 	x => console.log('success', x)		
+// );
+
+// Task.rejected(1)
+// .map(x => x + 1)
+// .fork(
+// 	e => console.log('error', e),
+// 	x => console.log('success', x)		
+// );
+
+// Task.of(1)
+// .map(x => x + 1)
+// .fork(
+// 	e => console.log('error', e),
+// 	x => console.log('success', x)		
+// );
+
+// Task.of(1)
+// .map(x => x + 1)
+// .chain(x => Task.of(x + 1))
+// .fork(
+// 	e => console.log('error', e),
+// 	x => console.log('success', x)		
+// );
+
+const launchSite = () =>
+	new Task((rej, res) => {
+		console.log('Launch site!');
+		res('site')
+	});
+
+// launchSite()
+// .map(x => x + '!')
+// .fork(
+// 	e => console.log('error', e),
+// 	x => console.log('success', x)		
+// );
+
+/* app deals with all of the side-effects */
+const app = launchSite().map(x => x + '!');
+app.map(x => x + '!').fork(
+	e => console.log('error', e),
+	x => console.log('success', x)		
+);
+
