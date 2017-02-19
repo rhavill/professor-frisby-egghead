7d280b9b9b89351f168735a138e140c377183f83
Ricardo Havill <ricardo.havill@mjfreeway.com>
2017-02-19 10:24:35 -0500

    Added lib folder.

--- 10-fold-map.js
+++ 10-fold-map.js
@@ -1,5 +1,5 @@
 const {Map, List} = require('immutable-ext');
-const {Sum} = require('./monoid');
+const {Sum} = require('./lib/monoid');
 
 const res = [Sum(1), Sum(2), Sum(3)]
 	.reduce((acc, x) => acc.concat(x), Sum.empty());
